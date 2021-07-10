1b20ba067c70b89835f36794090e8f5955b1e901
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-10 18:31:14 -0300

    Validar e-mail

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
+import { HttpException } from '@nestjs/common';
+
 export function trimObj(obj: object) {
   for (let key in obj) {
     if (typeof obj[key] === 'string') obj[key] = obj[key].trim();
   }
+}
+
+export function validateEmail(email: string) {
+  const regex = /\S+@\S+\.\S+/;
+
+  if (!regex.test(email)) throw new HttpException('E-mail inválido', 400);
 }
