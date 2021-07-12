022061cd05ed5fa7393a30588e9b2fdb18907f20
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-12 15:35:56 -0300

    Módulo de Upload e Media

---
+++ src/upload.service.ts
@@ -0,0 +1,27 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+
+@Injectable()
+export class UploadService {
+  s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  });
+
+  async uploadFile(file: Express.MulterS3.File) {
+    if (!file.mimetype.includes('image')) throw new HttpException('Arquivo não suportado', 400);
+
+    const upload = await this.s3.upload({
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: `${Date.now()}__${file.originalname}`,
+      Body: file.buffer,
+      ACL: 'public-read'
+    }).promise();
+
+    return {
+      mimetype: file.mimetype,
+      url: upload.Location,
+      name: upload.Key
+    };
+  }
+}
