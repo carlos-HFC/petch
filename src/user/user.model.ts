ab296a5598b9ef7025e80a383b88b50d177ba2f9
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-13 18:16:19 -0300

    Relacionamento do usuário com funções e mídias

--- src/user/user.model.ts
+++ src/user/user.model.ts
@@ -1,13 +1,19 @@
 import { compare, hash } from 'bcrypt';
 import { format, parseISO } from 'date-fns';
-import { BeforeSave, BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
-import { Role } from 'src/role/role.model';
+import { BeforeSave, BelongsTo, Column, DataType, DefaultScope, ForeignKey, Model, Table } from 'sequelize-typescript';
 
+import { Media } from '../medias/media.model';
+import { Role } from '../role/role.model';
+
+@DefaultScope(() => ({
+  include: [Role, Media]
+}))
 @Table({ paranoid: true })
 export class User extends Model {
   @Column({
     type: DataType.STRING,
-    unique: true
+    unique: true,
+    defaultValue: null
   })
   googleId: string;
 
@@ -27,6 +33,7 @@ export class User extends Model {
   @Column({
     type: DataType.BOOLEAN,
     allowNull: false,
+    defaultValue: false,
   })
   emailVerified: boolean;
 
@@ -97,10 +104,16 @@ export class User extends Model {
   })
   phone: string;
 
-  @Column(DataType.STRING)
+  @Column({
+    type: DataType.STRING,
+    defaultValue: null
+  })
   tokenResetPassword: string;
 
-  @Column(DataType.STRING)
+  @Column({
+    type: DataType.STRING,
+    defaultValue: null
+  })
   tokenResetPasswordExpires: string;
 
   @ForeignKey(() => Role)
@@ -110,14 +123,23 @@ export class User extends Model {
   @BelongsTo(() => Role)
   role: Role;
 
+  @ForeignKey(() => Media)
+  @Column({ allowNull: false })
+  mediaId: number;
+
+  @BelongsTo(() => Media)
+  media: Media;
+
   @BeforeSave
   static async hashPass(user: User) {
     if (user.password) return user.hash = await hash(user.password, 10);
   }
 
   @BeforeSave
-  static async setBirthday(user: User) {
-    return user.birthday = format(parseISO(user.birthday), 'yyyy-MM-dd');
+  static async formatData(user: User) {
+    user.birthday = format(parseISO(user.birthday), 'yyyy-MM-dd');
+    user.uf = user.uf.toUpperCase();
+    user.gender = user.gender.toUpperCase();
   }
 
   checkPass(password: string) {
