60e7e2aec44cc57730e4b554290ad3c6b373a671
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-14 13:35:38 -0300

    AuthGuard para JWT e Google

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Get, HttpCode, Post, Req, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 
+import { GoogleAuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -15,15 +15,16 @@ export class AuthController {
   async login(@Body() data: TLogin) {
     return await this.authService.login(data);
   }
+
   async forgot() { }
   async reset() { }
 
+  @UseGuards(GoogleAuthGuard)
   @Get('/google')
-  @UseGuards(AuthGuard('google'))
   async googleAuth(@Req() req: Request) { }
 
+  @UseGuards(GoogleAuthGuard)
   @Get('/google/redirect')
-  @UseGuards(AuthGuard('google'))
   async googleAuthRedirect(@Req() req: Request) {
     return await this.authService.googleLogin(req);
   }
