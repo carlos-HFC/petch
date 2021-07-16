54eee370e2460ce398a597d3136be7cce7bba4b1
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-16 18:02:20 -0300

    Novos campos e valor padrão

--- src/gift/gift.model.ts
+++ src/gift/gift.model.ts
@@ -8,17 +8,35 @@ export class Gift extends Model {
   })
   name: string;
 
-  @Column(DataType.STRING)
+  @Column({
+    type: DataType.STRING,
+    defaultValue: ''
+  })
   size: string;
 
   @Column({
     type: DataType.STRING,
-    allowNull: false,
+    defaultValue: ''
   })
   color: string;
 
+  @Column({
+    type: DataType.STRING,
+    defaultValue: ''
+  })
+  weight: string;
+
+  @Column({
+    type: DataType.STRING,
+    defaultValue: ''
+  })
+  taste: string;
+
   @BeforeSave
   static async formatSize(gift: Gift) {
-    gift.size = gift?.size.toUpperCase();
+    gift.size = gift.size?.toUpperCase();
+    gift.weight = gift.weight?.toUpperCase();
+    gift.color = gift.color?.charAt(0).toUpperCase() + gift.color?.slice(1);
+    gift.taste = gift.taste?.charAt(0).toUpperCase() + gift.taste?.slice(1);
   }
 }
--- src/gift/index.d.ts
+++ src/gift/index.d.ts
@@ -1,7 +1,9 @@
 type TCreateGift = {
   name: string;
-  color: string;
+  color?: string;
   size?: string;
+  weight?: string;
+  taste?: string;
 }
 
 type TUpdateGift = Partial<TCreateGift>
