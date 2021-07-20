2477eeb609cb525095035ccbcbf52040fffcbcc0
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-20 18:47:38 -0300

    Servidores, tag e bearer auth do Swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { AuthModule } from './auth/auth.module';
 import { GiftModule } from './gift/gift.module';
 import { OngModule } from './ong/ong.module';
 import { PartnerModule } from './partner/partner.module';
@@ -17,6 +18,10 @@ async function bootstrap() {
     .setTitle('Petch')
     .setDescription('Swagger do Petch')
     .setVersion('1.0.0')
+    .addServer('http://localhost:8000', 'Servidor de desenvolvimento')
+    .addServer('https://petch-teste.herokuapp.com', 'Servidor de produção')
+    .addBearerAuth({ type: 'http', in: 'header' })
+    .addTag('Auth', 'Autenticará os usuários, registrará novos adotantes, reiniciará a senha')
     .addTag('Gifts', 'Armazenará os brindes que o adotante escolherá ao adotar um pet')
     .addTag('ONGs', 'Armazenará as ONGs cadastradas pelo admin')
     .addTag('Partners', 'Armazenará os parceiros do Petch')
@@ -27,7 +32,7 @@ async function bootstrap() {
     .addTag('Users', 'Armazenará todos os usuários da plataforma')
     .build();
   const docs = SwaggerModule.createDocument(app, options, {
-    include: [GiftModule, OngModule, PartnerModule, RoleModule, SchedulingTypesModule, SolicitationTypesModule, SpeciesModule, UserModule],
+    include: [AuthModule, GiftModule, OngModule, PartnerModule, RoleModule, SchedulingTypesModule, SolicitationTypesModule, SpeciesModule, UserModule],
   });
   SwaggerModule.setup('swagger', app, docs);
   app.enableCors();
