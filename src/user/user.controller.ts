38fcf01475244f7f98153240f8226749c7dd12b5
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-21 18:01:28 -0300

    Swagger do Usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,13 @@
 import { Body, Controller, Get, Param, Patch, Post, Put, Query, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiCreatedResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiCreatedResponse, ApiForbiddenResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiQuery, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { Request } from 'express';
 
-import { User } from './user.model';
 import { UserService } from './user.service';
-import { CreateUser } from './user.swagger';
+import { CreateUser, UpdateUser, User } from './user.swagger';
 import { JwtAuthGuard } from '../auth/auth.guard';
+import { RoleGuard } from '../role/role.guard';
+import { RoleDecorator } from '../role/role.decorator';
 
 @ApiTags('Users')
 @Controller('users')
@@ -16,6 +17,9 @@ export class UserController {
   ) { }
 
   @ApiOkResponse({ type: [User], description: 'Success' })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @RoleDecorator('admin')
   @Get()
   async index() {
     return await this.userService.get();
@@ -39,6 +43,9 @@ export class UserController {
     }
   })
   @ApiParam({ name: 'id', required: true })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @RoleDecorator('admin')
   @Get(':id')
   async byId(@Param('id') id: number) {
     return await this.userService.findById(id);
@@ -72,15 +79,48 @@ export class UserController {
       }
     }
   })
+  @ApiUnauthorizedResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 401,
+        },
+        message: {
+          type: 'string',
+          example: 'Unauthorized'
+        }
+      }
+    }
+  })
+  @ApiForbiddenResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 403,
+        },
+        message: {
+          type: 'string',
+          example: 'Você não tem permissão'
+        }
+      }
+    }
+  })
   @ApiConsumes('multipart/form-data')
   @ApiBody({ type: CreateUser })
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @RoleDecorator('admin')
   @Post()
   @UseInterceptors(FileInterceptor('media'))
   async create(@Body() data: TCreateUser, @UploadedFile() media?: Express.MulterS3.File) {
-    return await this.userService.post(data, media);
+    return await this.userService.post(data, true, media);
   }
 
-  @ApiCreatedResponse({ type: User, description: 'Created' })
+  @ApiOkResponse({ description: 'Success' })
   @ApiBadRequestResponse({
     schema: {
       type: 'object',
@@ -100,7 +140,11 @@ export class UserController {
             { example: 'Número de telefone/celular inválido' },
             { example: 'Senha muito curta' },
             { example: 'Senha precisa ter uma letra maiúscula, uma letra minúscula, um caractere especial e um número' },
-            { example: 'Senhas não correspondem' },
+            { example: 'Senha atual incorreta' },
+            { example: 'Nova senha é obrigatória' },
+            { example: 'Nova senha não pode ser igual a senha atual' },
+            { example: 'Confirmação de senha é obrigatória' },
+            { example: 'Nova senha e confirmação de senha não correspondem' },
             { example: 'Data de nascimento inválida' },
             { example: 'Você não tem a idade mínima de 18 anos' },
           ]
@@ -108,10 +152,26 @@ export class UserController {
       }
     }
   })
+  @ApiUnauthorizedResponse({
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 401,
+        },
+        message: {
+          type: 'string',
+          example: 'Unauthorized'
+        }
+      }
+    }
+  })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({ type: CreateUser })
-  @Put()
+  @ApiBody({ type: UpdateUser })
+  @ApiBearerAuth()
   @UseGuards(JwtAuthGuard)
+  @Put()
   @UseInterceptors(FileInterceptor('media'))
   async update(@Req() req: Request, @Body() data: TUpdateUser, @UploadedFile() media?: Express.MulterS3.File) {
     return await this.userService.put(req.user, data, media);
--- src/user/user.swagger.ts
+++ src/user/user.swagger.ts
@@ -1,20 +1,29 @@
-import { ApiProperty } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 
-export class CreateUser {
-  @ApiProperty({ type: 'boolean', required: false, default: false })
-  isAdmin: boolean;
+export class User {
+  @ApiProperty({ type: 'integer', uniqueItems: true, readOnly: true })
+  id: number;
+
+  @ApiProperty({ type: 'string', uniqueItems: true, required: false })
+  googleId: string;
 
   @ApiProperty({ type: 'string' })
   name: string;
 
+  @ApiProperty({ type: 'string', required: false })
+  avatar: string;
+
   @ApiProperty({ type: 'string', uniqueItems: true })
   email: string;
 
-  @ApiProperty({ type: 'string' })
-  password: string;
+  @ApiProperty({ type: 'boolean', default: false })
+  emailVerified: boolean;
 
-  @ApiProperty({ type: 'string' })
-  confirmPassword: string;
+  @ApiProperty({ type: 'string', required: false })
+  tokenVerificationEmail: string;
+
+  @ApiProperty({ type: 'string', required: false })
+  hash: string;
 
   @ApiProperty({ type: 'string', uniqueItems: true })
   cpf: string;
@@ -34,6 +43,9 @@ export class CreateUser {
   @ApiProperty({ type: 'string' })
   district: string;
 
+  @ApiProperty({ type: 'string', required: false })
+  complement: string;
+
   @ApiProperty({ type: 'string' })
   city: string;
 
@@ -44,8 +56,36 @@ export class CreateUser {
   phone: string;
 
   @ApiProperty({ type: 'string', required: false })
-  complement: string;
+  tokenResetPassword: string;
+
+  @ApiProperty({ type: 'string', required: false })
+  tokenResetPasswordExpires: string;
+
+  @ApiProperty({ type: 'number' })
+  roleId: number;
+
+  @ApiProperty({ type: 'string', format: 'date', required: false, readOnly: true })
+  createdAt: Date;
+
+  @ApiProperty({ type: 'string', format: 'date', required: false, readOnly: true })
+  updatedAt: Date;
+
+  @ApiProperty({ type: 'string', format: 'date', required: false, readOnly: true })
+  deletedAt: Date | null;
+}
+
+export class CreateUser extends OmitType(User, ['createdAt', 'updatedAt', 'deletedAt', 'id', 'hash', 'avatar', 'tokenResetPassword', 'tokenResetPasswordExpires', 'tokenVerificationEmail', 'emailVerified', 'roleId']) {
+  @ApiProperty({ type: 'string' })
+  password: string;
+
+  @ApiProperty({ type: 'string' })
+  confirmPassword: string;
 
   @ApiProperty({ type: 'string', format: 'binary', required: false })
   media: string;
+}
+
+export class UpdateUser extends OmitType(PartialType(CreateUser), ['googleId']) {
+  @ApiProperty({ type: 'string', required: false })
+  oldPassword: string;
 }
