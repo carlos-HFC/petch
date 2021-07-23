b2cd2690bd7e40ff3d4601f1ae2fb2d5248d634e
Carlos Faustino <chfcchfc96@gmail.com>
2021-07-23 18:28:57 -0300

    Swagger para autenticação

---
+++ src/auth/auth.swagger.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class Login {
+  @ApiProperty({ type: 'string' })
+  email: string;
+
+  @ApiProperty({ type: 'string' })
+  password: string;
+}
+
+export class GoogleLogin {
+  @ApiProperty({ type: 'string' })
+  email: string;
+
+  @ApiProperty({ type: 'string' })
+  googleId: string;
+
+  @ApiProperty({ type: 'string' })
+  name: string;
+
+  @ApiProperty({ type: 'string', required: false })
+  avatar: string;
+}
+
+export class ResetPassword extends Login {
+  @ApiProperty({ type: 'string' })
+  token: string;
+
+  @ApiProperty({ type: 'string' })
+  confirmPassword: string;
+}
