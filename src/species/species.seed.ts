ba0a7a55c2083972e273b22b10dfa555751cb4b9
Carlos Faustino <chfcchfc96@gmail.com>
2021-08-02 18:13:52 -0300

    Campo novo para avatar

--- src/species/species.seed.ts
+++ src/species/species.seed.ts
@@ -9,8 +9,14 @@ import { Species } from './species.model';
 export class SpeciesSeed implements OnSeederInit {
   run() {
     return [
-      { name: "Cachorro" },
-      { name: "Gato" },
+      {
+        name: "Cachorro",
+        avatar: "https://images.unsplash.com/photo-1543466835-00a7907e9de1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      },
+      {
+        name: "Gato",
+        avatar: "https://images.unsplash.com/photo-1511044568932-338cba0ad803?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2F0fGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+      },
     ];
   }
 }
