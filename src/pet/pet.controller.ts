18c51107aff218574311c212b428748cd65ee7d7
Carlos Faustino <chfcchfc96@gmail.com>
2021-09-28 18:13:16 -0300

    Adoção, filtro de busca, cadastro, atualização

--- src/pet/pet.controller.ts
+++ src/pet/pet.controller.ts
@@ -1,23 +1,62 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Put, Query, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Put, Query, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
-import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiCreatedResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiCreatedResponse, ApiForbiddenResponse, ApiNoContentResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
+import { Request } from 'express';
 
+import { Pet, TCreatePet, TFilterPet, TUpdatePet } from './pet.dto';
 import { PetService } from './pet.service';
-import { CreatePet, FilterPet, IndexPet, Pet, UpdatePet } from './pet.swagger';
+import { RoleDecorator } from '../common/decorators/role.decorator';
+import { JwtAuthGuard } from '../common/guards/auth.guard';
+import { RoleGuard } from '../common/guards/role.guard';
 import { config } from '../config/multer';
 
 @ApiTags('Pets')
+@ApiUnauthorizedResponse({
+  description: 'Unauthorized',
+  schema: {
+    type: 'object',
+    properties: {
+      statusCode: {
+        type: 'number',
+        example: 401,
+      },
+      message: {
+        type: 'string',
+        example: 'Unauthorized'
+      }
+    }
+  }
+})
+@ApiForbiddenResponse({
+  description: 'Forbidden',
+  schema: {
+    type: 'object',
+    properties: {
+      statusCode: {
+        type: 'number',
+        example: 403,
+      },
+      message: {
+        type: 'string',
+        example: 'Você não tem permissão'
+      }
+    }
+  }
+})
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RoleGuard)
 @Controller('pets')
 export class PetController {
   constructor(
     private petService: PetService
   ) { }
 
-  @ApiOperation({ summary: 'Listar todos os pets' })
-  @ApiOkResponse({ type: [IndexPet], description: 'Success' })
-  @ApiQuery({ type: FilterPet, required: false })
+  @ApiOperation({ summary: 'Procurar pet para adoção e listar todos os pets' })
+  @ApiOkResponse({ type: [Pet], description: 'Success' })
+  @ApiQuery({ type: TFilterPet, required: false })
   @Get()
-  async index(@Query() query?: TFilterPet) {
+  async index(@Req() req: Request, @Query() query?: TFilterPet) {
+    if (req.user.role.name === 'Adotante') return await this.petService.find(query);
     return await this.petService.get(query);
   }
 
@@ -49,6 +88,7 @@ export class PetController {
   @ApiOperation({ summary: 'Cadastrar um pet' })
   @ApiCreatedResponse({ type: Pet, description: 'Created' })
   @ApiBadRequestResponse({
+    description: 'Bad Request',
     schema: {
       type: 'object',
       properties: {
@@ -60,8 +100,9 @@ export class PetController {
           type: 'string',
           oneOf: [
             { example: 'Arquivo não suportado' },
-            { example: 'Campo "X" não pode ser vazio' },
-            { example: 'O Pet deve conter uma foto' },
+            { example: 'Campo "X" é obrigatório' },
+            { example: 'Idade inválida' },
+            { example: 'Gênero inválido' },
           ]
         },
       }
@@ -87,16 +128,66 @@ export class PetController {
     }
   })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({ type: CreatePet })
+  @ApiBody({ type: TCreatePet })
+  @RoleDecorator('admin')
   @Post()
   @UseInterceptors(FileInterceptor('media', process.env.NODE_ENV === 'dev' ? config : {}))
   async create(@Body() data: TCreatePet, @UploadedFile() media: Express.MulterS3.File) {
-    return await this.petService.post(data, media);
+    return await this.petService.create(data, media);
+  }
+
+  @ApiOperation({ summary: 'Adotar um pet' })
+  @ApiCreatedResponse({ type: Pet, description: 'Created' })
+  @ApiBadRequestResponse({
+    description: 'Bad Request',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 400,
+        },
+        message: {
+          type: 'string',
+          oneOf: [
+            { example: 'Arquivo não suportado' },
+            { example: 'Campo "X" é obrigatório' },
+            { example: 'Idade inválida' },
+            { example: 'Gênero inválido' },
+          ]
+        },
+      }
+    }
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found',
+    schema: {
+      type: 'object',
+      properties: {
+        statusCode: {
+          type: 'number',
+          example: 404,
+        },
+        message: {
+          type: 'string',
+          oneOf: [
+            { example: 'ONG não encontrada' },
+            { example: 'Espécie não encontrada' },
+          ]
+        },
+      }
+    }
+  })
+  @RoleDecorator('adotante')
+  @Patch(':id')
+  async adopt(@Req() req: Request, @Param('id') id: number) {
+    return await this.petService.adopt(req.user, id);
   }
 
   @ApiOperation({ summary: 'Editar um pet' })
   @ApiOkResponse({ description: 'Success' })
   @ApiBadRequestResponse({
+    description: 'Bad Request',
     schema: {
       type: 'object',
       properties: {
@@ -108,8 +199,9 @@ export class PetController {
           type: 'string',
           oneOf: [
             { example: 'Arquivo não suportado' },
-            { example: 'Campo "X" não pode ser vazio' },
-            { example: 'O Pet deve conter uma foto' },
+            { example: 'Campo "X" é obrigatório' },
+            { example: 'Idade inválida' },
+            { example: 'Gênero inválido' },
           ]
         },
       }
@@ -127,6 +219,7 @@ export class PetController {
         message: {
           type: 'string',
           oneOf: [
+            { example: 'Pet não encontrado' },
             { example: 'ONG não encontrada' },
             { example: 'Espécie não encontrada' },
           ]
@@ -134,55 +227,40 @@ export class PetController {
       }
     }
   })
-  @ApiNotFoundResponse({
-    description: 'Not Found',
-    schema: {
-      type: 'object',
-      properties: {
-        statusCode: {
-          type: 'number',
-          example: 404,
-        },
-        message: {
-          type: 'string',
-          example: 'Pet não encontrado',
-        },
-      }
-    }
-  })
   @ApiConsumes('multipart/form-data')
-  @ApiBody({ type: UpdatePet })
+  @ApiBody({ type: TUpdatePet })
   @ApiParam({ name: 'id', required: true })
+  @RoleDecorator('admin')
   @Put(':id')
   @UseInterceptors(FileInterceptor('media', process.env.NODE_ENV === 'dev' ? config : {}))
-  async update(@Param('id') id: number, @Body() data: TCreatePet, @UploadedFile() media: Express.MulterS3.File) {
-    return await this.petService.put(id, data);
+  async update(@Param('id') id: number, @Body() data: TUpdatePet, @UploadedFile() media: Express.MulterS3.File) {
+    return await this.petService.put(id, data, media);
   }
 
-  @ApiOperation({ summary: 'Reativar um pet' })
-  @ApiNoContentResponse({ description: 'No Content' })
-  @ApiNotFoundResponse({
-    description: 'Not Found',
-    schema: {
-      type: 'object',
-      properties: {
-        statusCode: {
-          type: 'number',
-          example: 404,
-        },
-        message: {
-          type: 'string',
-          example: 'Pet não encontrado',
-        },
-      }
-    }
-  })
-  @ApiParam({ name: 'id', required: true })
-  @Patch(':id')
-  @HttpCode(204)
-  async restore(@Param('id') id: number) {
-    return await this.petService.restore(id);
-  }
+  // @ApiOperation({ summary: 'Reativar um pet' })
+  // @ApiNoContentResponse({ description: 'No Content' })
+  // @ApiNotFoundResponse({
+  //   description: 'Not Found',
+  //   schema: {
+  //     type: 'object',
+  //     properties: {
+  //       statusCode: {
+  //         type: 'number',
+  //         example: 404,
+  //       },
+  //       message: {
+  //         type: 'string',
+  //         example: 'Pet não encontrado',
+  //       },
+  //     }
+  //   }
+  // })
+  // @ApiParam({ name: 'id', required: true })
+  // @Patch(':id')
+  // @HttpCode(204)
+  // async restore(@Param('id') id: number) {
+  //   return await this.petService.restore(id);
+  // }
 
   @ApiOperation({ summary: 'Inativar um pet' })
   @ApiNoContentResponse({ description: 'No Content' })
--- src/pet/pet.service.ts
+++ src/pet/pet.service.ts
@@ -1,14 +1,15 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
-import { col, where } from 'sequelize';
+import { col, Op as $, where } from 'sequelize';
 import { Sequelize } from 'sequelize-typescript';
 
+import { TCreatePet, TFilterPet, TUpdatePet } from './pet.dto';
 import { Pet } from './pet.model';
 import { UploadService } from '../config/upload.service';
 import { OngService } from '../ong/ong.service';
-import { SizeService } from '../size/size.service';
 import { SpeciesService } from '../species/species.service';
-import { trimObj } from '../utils';
+import { User } from '../user/user.model';
+import { convertBool, trimObj } from '../utils';
 
 @Injectable()
 export class PetService {
@@ -17,23 +18,37 @@ export class PetService {
     private readonly petModel: typeof Pet,
     private ongService: OngService,
     private speciesService: SpeciesService,
-    private sizeService: SizeService,
     private uploadService: UploadService,
     private sequelize: Sequelize
   ) { }
 
+  async find(query?: TFilterPet) {
+    trimObj(query);
+    const options = {
+      userId: null
+    };
+
+    if (query.age) Object.assign(options, { age: { [$.startsWith]: query.age } });
+    if (query.weight) Object.assign(options, { weight: { [$.startsWith]: query.weight } });
+    if (query.cut) Object.assign(options, { cut: convertBool(query.cut) });
+    if (query.gender) Object.assign(options, { gender: query.gender });
+    if (query.speciesId) Object.assign(options, { speciesId: query.speciesId });
+    if (query.uf) Object.assign(options, { ong: where(col('ong.coverage'), { [$.substring]: query.uf.toUpperCase() }) });
+
+    return await this.petModel.findAll({
+      where: options,
+      order: this.sequelize.random(),
+      include: { all: true }
+    });
+  }
+
   async get(query?: TFilterPet) {
     trimObj(query);
 
-    const options = {};
-
-    if (query.gender) Object.assign(options, { gender: query.gender });
-    if (query.ong) Object.assign(options, { ong: where(col('ong.name'), query.ong.normalize()) });
-    if (query.species) Object.assign(options, { species: where(col('species.name'), query.species.normalize()) });
-
     return await this.petModel.findAll({
-      paranoid: !query.inactives,
-      where: options,
+      paranoid: !convertBool(query.inactives),
+      attributes: ['id', 'name', 'gender', 'age', 'deletedAt'],
+      order: [['id', 'asc']],
     });
   }
 
@@ -45,20 +60,23 @@ export class PetService {
     return pet;
   }
 
-  async post(data: TCreatePet, media: Express.MulterS3.File) {
+  async create(data: TCreatePet, media: Express.MulterS3.File) {
     trimObj(data);
     const transaction = await this.sequelize.transaction();
 
+    const num = (/([\d])/g);
+
     try {
-      if (data.ongId) await this.ongService.findById(data.ongId);
-      if (data.speciesId) await this.speciesService.findById(data.speciesId);
+      await this.ongService.findById(data.ongId);
+      await this.speciesService.findById(data.speciesId);
 
-      if (!media) throw new HttpException('O Pet deve conter uma foto', 400);
+      if (!media) throw new HttpException('Imagem é obrigatória', 400);
 
-      if (media) {
-        const image = (await this.uploadService.uploadFile(media)).url
-        Object.assign(data,{image})
-      }
+      const image = (await this.uploadService.uploadFile(media)).url;
+      Object.assign(data, { image });
+
+      const age = Number(data.age.match(num)?.join(''));
+      if (isNaN(age)) throw new HttpException('Idade inválida', 400);
 
       const pet = await this.petModel.create({ ...data }, { transaction });
 
@@ -71,9 +89,11 @@ export class PetService {
     }
   }
 
-  async put(id: number, data: TUpdatePet) {
+  async put(id: number, data: TUpdatePet, media: Express.MulterS3.File) {
     trimObj(data);
     const transaction = await this.sequelize.transaction();
+
+    const num = (/([\d])/g);
 
     try {
       const pet = await this.findById(id);
@@ -81,9 +101,43 @@ export class PetService {
       if (data.ongId) await this.ongService.findById(data.ongId);
       if (data.speciesId) await this.speciesService.findById(data.speciesId);
 
+      if (data.age) {
+        const age = Number(data.age.match(num)?.join(''));
+        if (isNaN(age)) throw new HttpException('Idade inválida', 400);
+      }
+
+      if (media) {
+        const image = (await this.uploadService.uploadFile(media)).url;
+        Object.assign(data, { image });
+      }
+
       await pet.update({ ...data }, { transaction });
 
       await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw new HttpException(error, 400);
+    }
+  }
+
+  async adopt(user: User, petId: number) {
+    const transaction = await this.sequelize.transaction();
+
+    try {
+      const pet = await this.petModel.findOne({
+        where: {
+          id: petId,
+          userId: null
+        }
+      });
+
+      if (!pet) throw new HttpException('Pet não encontrado', 404);
+
+      await pet.update({ userId: user.id }, { transaction });
+
+      await transaction.commit();
+
+      return { user, pet };
     } catch (error) {
       await transaction.rollback();
       throw new HttpException(error, 400);
